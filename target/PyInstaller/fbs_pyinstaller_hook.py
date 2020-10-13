3983149db68e78ea1b954a4af9c94a11878e3caf
Mario Rodriguez <marior6@uci.edu>
2020-10-13 08:32:10 -0500

    something is wrong with fbs installer and not including ddls

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'shopAutomatic', 'author': 'Mario Rodriguez', 'version': '1.0.0', 'environment': 'local'}
