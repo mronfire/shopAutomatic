ced6324fed8e47fff08678c79ba551307802dbe8
Mario Rodriguez <marior6@uci.edu>
2020-10-14 22:30:12 -0500

    Start ignoring *.py files

--- src/main/python/util/globales.py
+++
@@ -1,5 +0,0 @@
-new_tab = False  # Global variable to hold value to create new tabs
-tab_num = 1      # 
-driver  = None   # Global variable to hold the main driver
-driver_path = ""
-login   = False  #
